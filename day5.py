ab0ae291edede98abbfd24701f34799b1ccc892b
Pavanzzzz <psai84195@gmail.com>
2026-03-24 13:11:45 +0530

    SNOW

---
+++ day5.py
@@ -0,0 +1,41 @@
+'''string-->
+
+
+num = 786
+ss = '8976'
+for i in ss:
+    print(i)
+
+print (ss[1:3:])'''
+'''
+b = 'pavan sai'
+print(b[7::-1])
+
+
+any = 'chennai super kings'
+any = 'kkk'
+print(any)
+so = any.replace('k','pavan')
+print(so)'''
+'''
+
+p = "I'd never do that to you as I'm Pavan "
+print(f"my name is {p[4:]}")
+#if you do not give a value before or after the column by default it takes the end of it
+'''
+'''
+p = "pavan"
+p= (p)
+print(p)
+print(len(p))'''
+
+p = "python is a programming language"
+print(p.split('n'))
+#variable.split() splits the string based on whats put in the brackets of split if nothing is put it splits based on space by default
+#variable.lower() would make all of the string in small letters .upper() would turn them into capital letters
+print(p[12:23].upper())
+#.replace("the thing you want to replace from the string","the thingyou want to replace with")
+if "python" in p: print (p)
+
+
+
