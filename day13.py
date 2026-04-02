269b1ed4f5e3b0f06bbb23649b8267b6727ca03a
Pavanzzzz <psai84195@gmail.com>
2026-04-02 12:52:31 +0530

    ferrari

---
+++ day13.py
@@ -0,0 +1,37 @@
+'''for j in range(1,10):
+    print(j)
+    if j ==5:
+        break
+
+
+for j in range(2,20):
+    print(j)
+    if j == 5:
+        continue
+    print(n)
+
+
+
+for j in range(1,100):
+    pass
+    
+
+for m in range(1,10):
+    print(m)
+else:
+    print("else ")
+
+while---> this a combination of for and if  statements,if we did not end the loop
+in proper way
+
+
+num = 1
+while num<5:
+    print(5)'''
+n = int(input())
+n1 = 0
+n2 = 1
+for i in range(n):
+    n3 = n1+n2
+    print(n3)
+    n2,n1 = n3,n2
